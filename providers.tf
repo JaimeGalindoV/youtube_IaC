c071306804b101251a2e88d641d023841024c4b9
Jaime Galindo <jaime.galindo@iteso.mx>
2026-05-12 20:19:47 -0600

    Agregación de back para tener sincronizado el estado de terraform

--- providers.tf
+++ providers.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket  = "ourtube-iac"
+    key     = "back/terraform.tfstate"
+    region  = "us-east-1"
+  }
 }
 
 provider "aws" {
